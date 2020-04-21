562b9ba8ec1f54e48e9529e7c283291248f42a99
Marco Endrizzi <me00531@surrey.ac.uk>
2020-04-21 20:41:20 +0200

    Variables are now final, better order for closeConnection() and constructor is private

--- assignment/src/main/java/com/com1028/assignment/DatabaseConnection.java
+++ assignment/src/main/java/com/com1028/assignment/DatabaseConnection.java
@@ -9,56 +9,52 @@ import org.apache.commons.dbutils.DbUtils;
 
 public class DatabaseConnection {
   	
-    private static DatabaseConnection instance = null;
-	private static Connection connection = null;
-	private static Statement statement = null;
-	private static ResultSet rs = null;
-	private String username = "root";
-	private String password = "";
-	private final String db = "jdbc:mysql://localhost:3306/classicmodels?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+  private static DatabaseConnection instance = null;
+  private static Connection connection = null;
+  private static Statement statement = null;
+  private static ResultSet rs = null;
+  private final static String username = "root";
+  private final static String password = "";
+  private final static String db = "jdbc:mysql://localhost:3306/classicmodels?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
-	public DatabaseConnection(){
-	  try {
-		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
-		connection = DriverManager.getConnection(db, username, password);
-	  }
-	  catch(Exception e) {
-		System.out.println(e);
-	  }
+  private DatabaseConnection() throws SQLException {
+	try {
+	  DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
+	  connection = DriverManager.getConnection(db, username, password);
 	}
-	
-	protected ResultSet useTable(String tableName) throws SQLException{
-	  String query = "select * from " + tableName; 
-	  statement = connection.createStatement();
-	  rs = statement.executeQuery(query);
-	  return rs;
+	catch(Exception e) {
+	  System.out.println("Failed to get connection");
 	}
-	
-    public Connection getConnection() {
-      return connection;
   }
 	
-	public static DatabaseConnection getInstance() throws SQLException {
-	  if (instance == null) {
-		instance = new DatabaseConnection();
-	  } 
-	  else if (instance.getConnection().isClosed()) {
-		instance = new DatabaseConnection();
-	  }
-	  return instance;
+  protected ResultSet useTable(String tableName) throws SQLException{
+	String query = "select * from " + tableName; 
+	statement = connection.createStatement();
+	rs = statement.executeQuery(query);
+	return rs;
+  }
+	
+  public Connection getConnection() {
+    return connection;
+  }
+	
+  public static DatabaseConnection getInstance() throws SQLException {
+	if (instance == null) {
+	  instance = new DatabaseConnection();
+	} 
+	else if (instance.getConnection().isClosed()) {
+	  instance = new DatabaseConnection();
+	}
+	return instance;
 	}
 	
-	public static void closeConnection() {
-	  try {
-		DbUtils.close(connection);
-		DbUtils.close(statement);
-		DbUtils.close(rs);
-	  } catch (SQLException e) {
-		e.printStackTrace();
-	  }
-
+  public static void closeConnection() {
+	try {
+	  DbUtils.close(connection);
+	  DbUtils.close(statement);
+	  DbUtils.close(rs);
+	} catch (SQLException e) {
+	  System.out.println("Failed to close connection");
 	}
-	
-	
-
+  }
 }
